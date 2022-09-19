cf0c84482577f6ac39f9ca039d025b81d09c0199
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-19 17:50:05 +0000

    Mon, Sep 19, 2022, 11:20 PM +05:30

---
+++ faces/faces.py
@@ -0,0 +1,9 @@
+# Ask the user for their input
+usrinput = input("Whats happening? ")
+
+# replace the white space with ...
+usrinput = usrinput.replace(":)", "🙂")
+usrinput = usrinput.replace(":(", "🙁")
+
+# Print the output
+print(f"{usrinput}")
