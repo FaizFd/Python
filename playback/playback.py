5b72847a4684eb5dc863dc2dcf4a08f386f42f87
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-19 17:41:34 +0000

    Mon, Sep 19, 2022, 11:11 PM +05:30

---
+++ playback/playback.py
@@ -0,0 +1,8 @@
+# Ask the user for their input
+usrinput = input("Whats happening? ")
+
+# replace the white space with ...
+usrinput = usrinput.replace(" ", "...")
+
+# Print the output
+print(f"{usrinput}")
