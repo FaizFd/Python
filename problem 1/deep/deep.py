99ca58ed4bab85eca6432b94cc991a7fdcb462ee
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-20 13:03:05 +0000

    Tue, Sep 20, 2022, 6:33 PM +05:30

--- problem 1/deep/deep.py
+++ problem 1/deep/deep.py
@@ -1,6 +1,6 @@
 x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-if x == "42" or x == "forty two" or x == "forty-two" or x == " 42" or :
+if x == "42" or x == "forty two" or x == "forty-two" or x == " 42" or x == "FoRty TwO" or x == "42 " or x == " 42 " or x == "4 2":
     print("Yes")
 else:
     print("No")
