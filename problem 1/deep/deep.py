5c9848d3f175840241d5e927a2adeb53a012cc36
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-20 13:14:27 +0000

    Tue, Sep 20, 2022, 6:44 PM +05:30

--- problem 1/deep/deep.py
+++ problem 1/deep/deep.py
@@ -1,12 +1,10 @@
 x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-if x == "42"
+if x.strip() == "42":
     print("Yes")
-elif:
-    x.lower().strip() == "forty two"
+elif x.lower().strip() == "forty two":
     print("Yes")
-elif:
-    x.lower().strip() == "forty-two"
+elif x.lower().strip() == "forty-two":
     print("Yes")
 else:
     print("No")
