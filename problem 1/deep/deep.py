480fdcd29aadcc8119a57b1e4db39bf0359934c3
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-20 12:54:51 +0000

    Tue, Sep 20, 2022, 6:24 PM +05:30

--- problem 1/deep/deep.py
+++ problem 1/deep/deep.py
@@ -1,4 +1,4 @@
-x = int(input("What is the Answer to the Great Question of Life, the Universe, and Everything? "))
+x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
 if x == 42 or x == "forty two" or x == "forty-two":
     print("Yes")
