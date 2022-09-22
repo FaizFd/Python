6ab6996981d8625eedf877b75fbf7bf8cfebd22c
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-22 15:25:31 +0000

    Thu, Sep 22, 2022, 8:55 PM +05:30

--- problem 1/extentions/extentions.py
+++ problem 1/extentions/extentions.py
@@ -4,4 +4,13 @@ new_name = name.lower().strip()
 
 if ".gif" in new_name:
     print("image/gif")
-elif ".jpg"
+elif ".jpg" in new_name:
+    print("image/jpg")
+elif  ".jpeg" in new_name:
+    print("image/jpeg")
+elif ".png" in new_name:
+    print("image/png")
+elif ".pdf" in new_name:
+    print("application/pdf")
+elif ".txt" in new_name:
+    print("")
