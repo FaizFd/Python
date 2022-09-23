c88ed9ac9240b8efb076b56e526a03f6efbdd10e
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-23 13:44:17 +0000

    Fri, Sep 23, 2022, 7:14 PM +05:30

--- problem 1/interpreter/interpreter.py
+++ problem 1/interpreter/interpreter.py
@@ -1,3 +1,16 @@
-x, y, z = int(input("Expression: "))
+expression = input("Expression: ")
 
 x, y, z = expression.split(" ")
+
+new_x = float(x)
+new_z = float(z)
+
+if y == "+":
+    result = new_x + new_z
+if y == "-":
+    result = new_x + new_y
+if y == "*":
+    result = new_x * new_z
+if y == "/":
+    result = new_x / new_z
+print (result)
