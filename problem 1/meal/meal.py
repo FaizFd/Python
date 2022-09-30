bbdd3b1a1d37cfb1e7f82621e20b903408ec6251
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-09-30 12:52:19 +0000

    Fri, Sep 30, 2022, 6:22 PM +05:30

--- problem 1/meal/meal.py
+++ problem 1/meal/meal.py
@@ -1,10 +1,13 @@
 def main():
-    time = input("")
+    answer = input("What time is it? ")
+    time = convert(answer)
+    if time = 
 
 
 def convert(time):
-    ...
-
+    hours, minutes = time.split(":")
+    new_minute = float(minutes) / 60
+    return float(hours) + new_minute
 
 if __name__ == "__main__":
     main()
