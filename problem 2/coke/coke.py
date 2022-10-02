a18773b1b16e97f6facd698f86cbebe46ebcf95f
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-10-02 15:17:14 +0000

    Sun, Oct 2, 2022, 3:17 PM +00:00

--- problem 2/coke/coke.py
+++ problem 2/coke/coke.py
@@ -3,7 +3,7 @@ amount_due = 50
 while amount_due > 0:
     print("Amount Due: 50", amount_due)
     coin = int(input("Insert Coin: "))
-    if coin == 25 or coin == 10 or coin == 5:
+    if coin in [25, 10, 5]:
         amount_due -= coin
 
 change_owed = abs(amount_due)
