9c18af77e2573cf5bb75c8171d9057ee344378a1
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-12-09 09:30:17 +0000

    Fri, Dec 9, 2022, 9:30 AM +00:00

--- problem 2/plates/plates.py
+++ problem 2/plates/plates.py
@@ -14,7 +14,10 @@ def is_valid(s):
         return False
 
     i = 0
-    while i < len(s)
+    while i < len(s):
+        if s[i].isalpha == False:
+            if s[i] == '0':
+                return False
 
 
 main()
