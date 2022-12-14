ece916628d2a4bf540d8ed7493e8ca6a6284754d
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-12-14 06:40:00 +0000

    Wed, Dec 14, 2022, 6:40 AM +00:00

--- problem 3/fuel/fuel.py
+++ problem 3/fuel/fuel.py
@@ -12,4 +12,15 @@ while True:
         if f <= 1:
             break
 
-except ValueError
+except (ValueError, ZeroDivisionError):
+    pass
+
+p = int(f * 100)
+
+if p <= 1:
+    print("E")
+
+elif p >= 99:
+    print("F")
+
+else print(f"{p}%")
