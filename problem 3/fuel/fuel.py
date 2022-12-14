9eb8b7ede4e93c112990288647ff97a6da486b58
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-12-14 06:35:49 +0000

    Wed, Dec 14, 2022, 6:35 AM +00:00

--- problem 3/fuel/fuel.py
+++ problem 3/fuel/fuel.py
@@ -3,11 +3,13 @@ while True:
     fuel = input("Fraction: ")
     try:
         numerator, denominator = fuel.split("/")
+
         new_numerator = int(numerator)
         new_denominator = int(denominator)
+
         f = new_numerator / new_denominator
 
-except ValueError:
-    ...
-except ZeroDivisionError:
-    ...
+        if f <= 1:
+            break
+
+except ValueError
