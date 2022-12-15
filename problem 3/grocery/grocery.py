99fb0c6a26bbd3a577ef4cd02049acdb9f493188
FaizFd <69901327+FaizFd@users.noreply.github.com>
2022-12-15 09:15:02 +0000

    Thu, Dec 15, 2022, 9:15 AM +00:00

--- problem 3/grocery/grocery.py
+++ problem 3/grocery/grocery.py
@@ -3,4 +3,17 @@ grocery = {}
 while True:
 
     try:
-        
+        item = input()
+
+        if item in grocery:
+            grocery [item] += 1
+
+        else:
+            grocey [item] = 1
+
+     except EOFError:
+
+        for key in grocery:
+            print(key)
+
+        break
