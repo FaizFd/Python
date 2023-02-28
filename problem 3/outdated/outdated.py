703e53d231aebe134c29c8a1f7cf14cf05090197
FaizFd <69901327+FaizFd@users.noreply.github.com>
2023-02-28 08:00:48 +0000

    Tue, Feb 28, 2023, 8:00 AM +00:00

--- problem 3/outdated/outdated.py
+++ problem 3/outdated/outdated.py
@@ -14,3 +14,10 @@ months = [
 ]
 
 while true:
+    date = input("Date: ")
+    try:
+        if (int(month) >= 1 and int(month) <= 12) and (int(day) >=1 and int(day) <= 31):
+            break
+    except:
+        try:
+            old_month, old_day, year = date.split(" ")
