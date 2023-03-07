e353d2ea4add0885a09acad0fb93b298e055bcbe
FaizFd <69901327+FaizFd@users.noreply.github.com>
2023-03-07 07:15:26 +0000

    Tue, Mar 7, 2023, 7:15 AM +00:00

--- problem 3/outdated/outdated.py
+++ problem 3/outdated/outdated.py
@@ -16,25 +16,25 @@ def main():
     while True:
         date = input('Date: ')
         try:
-            mnth,day,year = date.split('/')
-            mnth = int(mnth)
+            month,day,year = date.split('/')
+            month = int(month)
             day = int(day)
             year = int(year)
-            if (int (mnth) > 0 and int(mnth) < 13) and (int(day) > 0 and int(day) < 32):
+            if (int (month) > 0 and int(month) < 13) and (int(day) > 0 and int(day) < 32):
                 if year >= 1000 and year <= 9999:
-                    print (year, f'{mnth:02}',f'{day:02}', sep='-')
+                    print (year, f'{month:02}',f'{day:02}', sep='-')
                     break
         except:
             try:
                 if "," in date:
-                    mnth,day,year = date.split(' ')
+                    month,day,year = date.split(' ')
                     day = day.replace(',','')
                     day = int(day)
                     year = int(year)
-                    if mnth in months and (int(day) > 0 and int(day) < 32):
+                    if month in months and (int(day) > 0 and int(day) < 32):
                         if year >= 1000 and year <= 9999:
-                            mnth= (months.index (mnth)+1)
-                            print (year, f'{mnth:02}',f'{day:02}', sep = '-')
+                            month= (months.index (month)+1)
+                            print (year, f'{month:02}',f'{day:02}', sep = '-')
                             break
             except:
                 pass
