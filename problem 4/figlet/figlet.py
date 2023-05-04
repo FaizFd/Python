edfb2fc5878144c95204bdeaf69d0b54bc1448e5
FaizFd <69901327+FaizFd@users.noreply.github.com>
2023-05-04 18:51:45 +0000

    Fri, May 5, 2023, 12:21 AM +05:30

--- problem 4/figlet/figlet.py
+++ problem 4/figlet/figlet.py
@@ -1,30 +1,24 @@
-import sys
 from pyfiglet import Figlet
+import sys
 import random
 
 figlet = Figlet()
 
-if len(sys.argv) == 1:
-    isRandomFont = True
-elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
-    isRandomFont = False
+
+if len(sys.argv) == 3:
+
+    if (sys.argv[1] == '-f' or sys.argv[1] == '--font'):
+        for font in figlet.getFonts():
+            figlet.setFont(font=sys.argv[2])
+            prompt = input('Input: ')
+            print('Output:\n', figlet.renderText(prompt))
+            break
+    else:
+        sys.exit('Invalid usage')
+
+elif len(sys.argv) < 2:
+    figlet.setFont(font=random.choice(figlet.getFonts()))
+    response = input('Input: ')
+    print('Output:\n', figlet.renderText(response))
 else:
-    sys.exit(1)
-
-figlet.getFonts()
-
-if isRandomFont == False:
-    try:
-        figlet.setFont(font=sys.argv[2])
-        print(figlet.renderText(msg))
-    except:
-        print("Invalid usage")
-        sys.exit(1)
-
-else:
-    font = random.choice(figlet.getFonts())
-
-msg = input("Input: ")
-
-print("Output: ")
-print(figlet.renderText(msg))
+    sys.exit('Invalid usage')
