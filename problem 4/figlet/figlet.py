bbe2111fcc413a80a88ffa2d3675d633a475fa60
FaizFd <69901327+FaizFd@users.noreply.github.com>
2023-05-04 18:35:43 +0000

    Fri, May 5, 2023, 12:05 AM +05:30

--- problem 4/figlet/figlet.py
+++ problem 4/figlet/figlet.py
@@ -8,6 +8,7 @@ if len(sys.argv) == 1:
 elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
     isRandomFont = False
 else:
+    print("Invalid usage")
     sys.exit(1)
 
 figlet.getFonts()
@@ -17,7 +18,7 @@ if isRandomFont == False:
         figlet.setFont(font=sys.argv[2])
         print(figlet.renderText(msg))
     except:
-        print("Invalid Responce")
+        print("Invalid usage")
         sys.exit(1)
 
 
