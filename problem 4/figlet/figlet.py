4b0c5ea2b3e070e2a11fb7e90fe853581cef2fdb
FaizFd <69901327+FaizFd@users.noreply.github.com>
2023-05-04 18:17:36 +0000

    Thu, May 4, 2023, 11:47 PM +05:30

---
+++ problem 4/figlet/figlet.py
@@ -0,0 +1,10 @@
+import sys
+
+if len(sys.argv) == 1:
+    isRandomFont = True
+elif len(sys.argv) == 3 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    isRandomFont = False
+else:
+    sys.exit(1)
+
+print(isRandomFont)
